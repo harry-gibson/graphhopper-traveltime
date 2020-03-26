669e067df2ee32a0cf2095095da9b6cf8d6693c6
Camilo Vargas <cavargasru@map-fs1.ndph.ox.ac.uk>
2020-03-26 05:36:06 +0000

    Add mode as a parameter

--- src/main/java/uk/ac/ox/map/App.java
+++ src/main/java/uk/ac/ox/map/App.java
@@ -13,11 +13,12 @@ public class App
     public static void main( String[] args ) {
         String osmFile = args[0];
         String graphLocation = args[1];
+        String mode = args [2]
         GraphHopper hopper = new GraphHopperOSM().setOSMFile(osmFile).
                 setStoreOnFlush(true).
                 setCHEnabled(true).
                 setGraphHopperLocation(graphLocation).
-                setEncodingManager(EncodingManager.create("car,bike,foot")).
+                setEncodingManager(EncodingManager.create(mode)).
                 importOrLoad();
     }
 }
