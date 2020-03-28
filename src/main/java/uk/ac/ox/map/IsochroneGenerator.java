1480eaa2a31cae14f941a2dd85fc6540aec3b7df
Camilo Vargas <cavargasru@map-fs1.ndph.ox.ac.uk>
2020-03-28 15:17:17 +0000

    Add cache

--- src/main/java/uk/ac/ox/map/IsochroneGenerator.java
+++ src/main/java/uk/ac/ox/map/IsochroneGenerator.java
@@ -1,6 +1,7 @@
 package uk.ac.ox.map;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import com.graphhopper.GraphHopper;
 import com.graphhopper.isochrone.algorithm.ContourBuilder;
 import com.graphhopper.isochrone.algorithm.Isochrone;
@@ -24,16 +25,13 @@ import org.locationtech.jts.triangulate.ConstraintVertex;
 import org.locationtech.jts.triangulate.quadedge.QuadEdgeSubdivision;
 import org.locationtech.jts.triangulate.quadedge.Vertex;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class IsochroneGenerator {
@@ -43,13 +41,19 @@ public class IsochroneGenerator {
     private final static GeometryFactory geometryFactory = new GeometryFactory();
     private final static WKTWriter wktWriter = new WKTWriter();
 
+    private static final long MEGABYTE = 1024L * 1024L;
+
+    public static long bytesToMegabytes(long bytes) {
+        return bytes / MEGABYTE;
+    }
+
 
     public static void main( String[] args ) throws IOException {
 
         LocalDateTime start = LocalDateTime.now();
         System.out.println(dtf.format(start));
 
-
+        printMemoryUsage();
         int timeLimit = 8100;
         int numberOfBuckets = 9;
         // Network
@@ -59,8 +63,18 @@ public class IsochroneGenerator {
         GraphHopper hopper = getGraph(osmFile, graphLocation, mode);
         EncodingManager encodingManager = hopper.getEncodingManager();
         FlagEncoder encoder = encodingManager.getEncoder(mode);
+        // Cache
+        Reader inCache = new FileReader(args[4]);
+        Iterable<CSVRecord> cache = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(inCache);
+        Set<Coordinate> caches = Sets.newHashSet();
+        for (CSVRecord record : cache) {
+            Double lat = Double.parseDouble(record.get("lat"));
+            Double lon = Double.parseDouble(record.get("lon"));
+            Coordinate coordinate = new Coordinate(lon,lat);
+            caches.add(coordinate);
+        }
         // Origins
-        Reader in = new FileReader(args [3]);
+        Reader in = new FileReader(args[3]);
         Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
         List<CSVRecord> csvRecords = Lists.newArrayList();
         for (CSVRecord record : records) {
@@ -69,9 +83,12 @@ public class IsochroneGenerator {
             if(!latString.isEmpty() && !lonString.isEmpty()){
                 csvRecords.add(record);
             }
-
         }
-        FileWriter out = new FileWriter(args [4]);
+//        FileWriter out = new FileWriter(args[4]);
+        BufferedWriter out = Files.newBufferedWriter(
+                Paths.get(args[4]),
+                StandardOpenOption.APPEND,
+                StandardOpenOption.CREATE);
         CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT);
         printer.printRecord("time", "isochrone","lat","lon");
         IntStream.range(0, csvRecords.size()).parallel().forEach(
@@ -79,29 +96,34 @@ public class IsochroneGenerator {
                     CSVRecord record = csvRecords.get(i);
                     Double lat = Double.parseDouble(record.get("Lat"));
                     Double lon = Double.parseDouble(record.get("Long"));
-                    List<List<Coordinate>> isochrone = buildIsochrone(timeLimit, numberOfBuckets, hopper, encoder, lat, lon);
-                    if(isochrone != null){
-                        List<Coordinate[]> polygonShells = buildIsochronePolygons(lat, lon, isochrone);
-                        Polygon previousPolygon = geometryFactory.createPolygon(polygonShells.get(0));
-                        int interval = timeLimit / numberOfBuckets;
-                        try {
-                            printer.printRecord( interval , wktWriter.write(previousPolygon), record.get("Lat"), record.get("Long"));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        for (int j = 1; j < polygonShells.size() - 1; j++) {
-                            Polygon polygon = geometryFactory.createPolygon(polygonShells.get(j));
+                    Coordinate coordinate = new Coordinate(lon,lat);
+                    if(!caches.contains(coordinate)){
+                        List<List<Coordinate>> isochrone = buildIsochrone(timeLimit, numberOfBuckets, hopper, encoder, lat, lon);
+                        printMemoryUsage();
+                        if(isochrone != null){
+                            List<Coordinate[]> polygonShells = buildIsochronePolygons(lat, lon, isochrone);
+                            Polygon previousPolygon = geometryFactory.createPolygon(polygonShells.get(0));
+                            int interval = timeLimit / numberOfBuckets;
                             try {
-                                printer.printRecord( ((j+1) * interval) , wktWriter.write(polygon.difference(previousPolygon)), record.get("Lat"), record.get("Long"));
+                                printer.printRecord( interval , wktWriter.write(previousPolygon), record.get("Lat"), record.get("Long"));
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
-                            previousPolygon = polygon;
+                            for (int j = 1; j < polygonShells.size() - 1; j++) {
+                                Polygon polygon = geometryFactory.createPolygon(polygonShells.get(j));
+                                try {
+                                    printer.printRecord( ((j+1) * interval) , wktWriter.write(polygon.difference(previousPolygon)), record.get("Lat"), record.get("Long"));
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                                previousPolygon = polygon;
 
+                            }
                         }
                     }
                 }
         );
+        printMemoryUsage();
         printer.close();
 
         LocalDateTime end = LocalDateTime.now();
@@ -186,5 +208,17 @@ public class IsochroneGenerator {
             }
         }
         return maxPolygon;
+    }
+
+    private static void printMemoryUsage() {
+        // Get the Java runtime
+        Runtime runtime = Runtime.getRuntime();
+        // Run the garbage collector
+        runtime.gc();
+        // Calculate the used memory
+        long memory = runtime.totalMemory() - runtime.freeMemory();
+        System.out.println("Used memory is bytes: " + memory);
+        System.out.println("Used memory is megabytes: "
+                + bytesToMegabytes(memory));
     }
 }
