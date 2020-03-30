822f75d5aa8b1d2ef14e233bc3736dd4ed59b1e1
Camilo Vargas <cavargasru@map-fs1.ndph.ox.ac.uk>
2020-03-30 15:01:36 +0100

    update scripts

--- create-cluster.sh
+++ create-cluster.sh
@@ -2,9 +2,9 @@
 gcloud --project=map-visualization-dev beta dataproc clusters create \
    --num-workers=30 \
    --scopes=cloud-platform \
-   --worker-machine-type=n1-standard-1 \
+   --worker-machine-type=n1-standard-2 \
    --worker-boot-disk-type=pd-ssd \
-   --master-machine-type=n1-standard-1 \
+   --master-machine-type=n1-standard-2 \
    --region=europe-west1 \
    --zone=europe-west1-b \
    --max-idle=600 \
--- install-on-cluster.sh
+++ install-on-cluster.sh
@@ -2,3 +2,4 @@
 apt-get update -y
 mkdir -p /tmp
 gsutil -m cp -r gs://map-graphhopper/africa/car/network /tmp
+chmod -R 0777 /tmp/network
