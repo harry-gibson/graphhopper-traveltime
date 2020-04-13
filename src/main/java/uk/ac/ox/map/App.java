086a81bde36643e3cbbf909ae2e73717d08f1933
Camilo Vargas <cavargasru@map-fs1.ndph.ox.ac.uk>
2020-04-13 17:23:05 +0100

    Temporal

--- src/main/java/uk/ac/ox/map/App.java
+++ src/main/java/uk/ac/ox/map/App.java
@@ -5,11 +5,13 @@ import com.graphhopper.GraphHopper;
 import com.graphhopper.reader.osm.GraphHopperOSM;
 import com.graphhopper.routing.util.EncodingManager;
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 import org.locationtech.jts.io.ParseException;
 import org.locationtech.jts.io.WKBReader;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.List;
@@ -22,21 +24,31 @@ public class App
 {
     public static void main( String[] args ) throws IOException, ParseException {
         WKBReader wkbReader = new WKBReader();
-        Reader in = new FileReader("/home/cavargasru/Downloads/isochrones_1_16_part-m-00000");
+        Reader in = new FileReader("/home/cavargasru/Downloads/zwe_isochrones_900.csv");
         Iterable<CSVRecord> records = CSVFormat.RFC4180.parse(in);
         List<CSVRecord> csvRecords = Lists.newArrayList();
         int i = 0;
         System.out.println(csvRecords.size());
+
+
+
+
+        FileWriter out = new FileWriter("/home/cavargasru/Downloads/zwe_isochrones_wkt_900.csv");
+        CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT);
+        printer.printRecord("tid", "geom");
+
+
+
         for (CSVRecord record : records) {
 
             String geom = record.get(record.size() - 1);
 
             byte[] bytes = WKBReader.hexToBytes(geom);
 
-            System.out.println(i + " " + wkbReader.read(bytes).toText());
+            printer.printRecord(i , wkbReader.read(bytes).toText());
             i++;
         }
-
+        printer.close();
         System.exit(0);
 
         String osmFile = args[0];
