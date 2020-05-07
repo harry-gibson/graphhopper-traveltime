305365736f4fac55a47f3cfaeb6904415076ac61
Harry Gibson <harry.s.gibson@gmail.com>
2020-05-07 15:08:38 +0100

    update gitignore to exclude test data folder

---
+++ src/main/java/au/org/telethonkids/map/GraphhopperRunConfiguration.java
@@ -0,0 +1,8 @@
+package au.org.telethonkids.map;
+import io.dropwizard.Configuration;
+import com.graphhopper.util.CmdArgs;
+
+// see "C:\\Users\\zool1301\\Documents\\GitHub\\graphhopper\\api\\src\\main\\java\\com\\graphhopper\\util\\CmdArgs.java"
+// and "C:\\Users\\zool1301\\Documents\\GitHub\\graphhopper\\web\\src\\main\\java\\com\\graphhopper\\http\\GraphHopperServerConfiguration.java"
+public class GraphhopperRunConfiguration extends Configuration{
+}
---
+++ src/main/java/au/org/telethonkids/map/TravelTimeByPTODGenerator.java
@@ -0,0 +1,143 @@
+package au.org.telethonkids.map;
+
+
+import com.graphhopper.GHRequest;
+import com.graphhopper.GHResponse;
+import com.graphhopper.GraphHopper;
+import com.graphhopper.PathWrapper;
+import com.graphhopper.reader.gtfs.GraphHopperGtfs;
+import com.graphhopper.reader.gtfs.GtfsStorage;
+import com.graphhopper.reader.gtfs.PtFlagEncoder;
+import com.graphhopper.reader.gtfs.Request;
+import com.graphhopper.routing.Path;
+import com.graphhopper.routing.util.EncodingManager;
+import com.graphhopper.routing.util.FlagEncoder;
+import com.graphhopper.routing.util.FootFlagEncoder;
+import com.graphhopper.storage.GHDirectory;
+import com.graphhopper.storage.GraphHopperStorage;
+import com.graphhopper.storage.index.LocationIndex;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class TravelTimeByPTODGenerator {
+
+    private final static DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+    private static final long MEGABYTE = 1024L * 1024L;
+    public static long bytesToMegabytes(long bytes) {
+        return bytes / MEGABYTE;
+    }
+
+    public static void main( String[] args ) throws IOException {
+
+        System.out.println("Start: " + dtf.format(LocalDateTime.now()));
+        App.printMemoryUsage();
+        String osmFile = args[0];
+        String gtfsFile = args[1];
+        String graphLocation = args[2];
+        String originsFile = args[3];
+        String destinationsFile = args [4];
+        String outFile = args[5];
+        String outErrorsFile = outFile.replace(".csv", ".errors.csv");
+        // no convenience hopper.close() method on GraphHopperGtfs, so in order to properly close the
+        // storage and locationindex at the end we need to have a reference to them here so don't use a static
+        // method in App to create the hopper
+        final PtFlagEncoder ptFlagEncoder = new PtFlagEncoder();
+        EncodingManager encodingManager = EncodingManager.create(Arrays.asList(ptFlagEncoder, new FootFlagEncoder()), 8);
+        GHDirectory directory = GraphHopperGtfs.createGHDirectory(graphLocation);
+        GtfsStorage gtfsStorage = GraphHopperGtfs.createGtfsStorage();
+        GraphHopperStorage graphHopperStorage = GraphHopperGtfs.createOrLoad(directory, encodingManager, ptFlagEncoder,
+                    gtfsStorage, Collections.singleton(gtfsFile), Collections.singleton(osmFile));
+        LocationIndex locationIndex = GraphHopperGtfs.createOrLoadIndex(directory, graphHopperStorage);
+        // nb GraphHopperGtfs does not extend GraphHopper like GraphHopperOSM does
+        GraphHopperGtfs hopper = GraphHopperGtfs.createFactory(ptFlagEncoder, GraphHopperGtfs.createTranslationMap(),
+                    graphHopperStorage, locationIndex, gtfsStorage)
+                    .createWithoutRealtimeFeed();
+
+        App.printMemoryUsage();
+        System.out.println("Network loaded: " + dtf.format(LocalDateTime.now()));
+
+        Set<CSVRecord> origins = App.getLocations(originsFile);
+        Set<CSVRecord> destinations = App.getLocations(destinationsFile);
+
+        FileWriter out = new FileWriter(outFile);
+        CSVPrinter printer = new CSVPrinter(out, CSVFormat.DEFAULT);
+        FileWriter outErrors = new FileWriter(outErrorsFile);
+        CSVPrinter errorPrinter = new CSVPrinter(outErrors, CSVFormat.DEFAULT);
+        printer.printRecord("sa1_origin", "dzn_destination","transit");
+        errorPrinter.printRecord("sa1_origin", "origin_lat", "origin_lon",
+                "dzn_destination", "dest_lat", "dest_lon");
+        // pick a random thursday in april that isn't near anzac day or easter
+        Instant depTime = LocalDateTime.of(2016,4,14,6,30,0)
+                .atZone(ZoneId.of("Australia/Perth"))
+                .toInstant();
+        try {
+            origins.parallelStream().limit(100).forEach(
+                    origin -> {
+                        String start = origin.get("uid");
+                        Double startLon = Double.parseDouble(origin.get("lon"));
+                        Double startLat = Double.parseDouble(origin.get("lat"));
+                        for (CSVRecord destination : destinations
+                        ) {
+                            String end = destination.get("uid");
+                            Double endLon = Double.parseDouble(destination.get("lon"));
+                            Double endLat = Double.parseDouble(destination.get("lat"));
+                            Request req = new Request(startLat, startLon, endLat, endLon);
+                            req.setEarliestDepartureTime(depTime);
+                            //req.setMaxWalkDistancePerLeg(5000);
+                            req.setProfileQuery(true);
+                            try {
+                                GHResponse rsp = hopper.route(req);
+                                if (!rsp.hasErrors()) {
+                                    // filter to only routes that aren't walk-only
+                                    // avoid calling getAll multiple times as this is presumably a bit expensive
+                                    List<PathWrapper> responses = rsp.getAll();
+                                    Supplier<Stream<PathWrapper>> streamSupplier = () -> responses.stream().filter(p -> p.getLegs().size() > 1);
+                                    Stream<PathWrapper> includingTransit = streamSupplier.get();
+                                    if (includingTransit.findAny().isPresent()) {
+                                        long bestNonWalkingTime = streamSupplier.get()
+                                                .min(Comparator.comparing(PathWrapper::getTime))
+                                                .orElseThrow(NoSuchElementException::new)
+                                                .getTime();
+                                        try {
+                                            printer.printRecord(start, end, bestNonWalkingTime);
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                }
+                            } catch (com.graphhopper.util.exceptions.PointNotFoundException e) {
+                                try {
+                                    errorPrinter.printRecord(start, startLat, startLon, end, endLat, endLon);
+                                    continue;
+                                } catch (IOException ioException) {
+                                    ioException.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+            );
+        }
+        finally {
+            // ensure that the graph (and csvs, but that's not so important) are properly closed whilst troubleshooting
+            // otherwise it gets corrupted every time we get an exception in the routing and has to be re-made
+            printer.close();
+            errorPrinter.close();
+            graphHopperStorage.close();
+            locationIndex.close();
+        }
+        System.out.println("End: " + dtf.format(LocalDateTime.now()));
+        App.printMemoryUsage();
+    }
+}
